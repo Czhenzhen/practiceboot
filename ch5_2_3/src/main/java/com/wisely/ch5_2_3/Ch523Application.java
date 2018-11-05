57149369e5e8c7bf9ab9725382355ccfbdf17253
Administrator <18631375954@163.com>
2018-11-05 16:14:38 +0800

    spring boot 项目初始化

---
+++ ch5_2_3/src/main/java/com/wisely/ch5_2_3/Ch523Application.java
@@ -0,0 +1,15 @@
+package com.wisely.ch5_2_3;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch523Application {
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Ch523Application.class);
+        //app.setBannerMode(Banner.Mode.OFF);//关闭banner
+        app.run(args);
+    }
+}
